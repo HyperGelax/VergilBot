4481c936f0cd1b469f6d83415217e31113e5a900
HyperGelax <tim.korytkin@gmail.com>
2023-04-01 00:59:33 +0900

    it's a base

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import json
+
+intents = discord.Intents.default()
+
+client = commands.Bot(command_prefix='?', intents=intents)
+
+TOKEN = 'MTA5MTM1MTgwNjE5OTc0NjYxMQ.G2QTFt.oTlU5leTScmM5wY7MNkWso7u4D1pq1-XLHf14Q'
+
+try:
+    with open('sent_messages.json', 'r') as f:
+        sent_messages = json.loads(f.read())
+except FileNotFoundError:
+    sent_messages = {}
+
+with open('sent_messages.json', 'w') as f:
+    f.write(json.dumps(sent_messages))
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} успешно подключился к Discord серверу!')
+    for guild in client.guilds:
+        channel = discord.utils.get(guild.channels, name='основной')
+        if channel and str(guild.id) in sent_messages:
+            message = f"Я был успешно перерожден и готов к работе на сервере {guild.name}."
+            sent_messages[str(guild.id)] = message
+            await channel.send(message)
+
+        elif channel:
+            await channel.send(f'`Покажи мне свою мотивацию! \n'
+                               f' "?help" для вызова помощи`')
+
+        with open('sent_messages.json', 'w') as f:
+            f.write(json.dumps(sent_messages))
+
+
+client.run(TOKEN)
